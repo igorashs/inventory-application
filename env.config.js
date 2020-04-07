087ad04f868baa1a033abde7eb0cdf56d5109971
igorashs <igorashsirbu@gmail.com>
2020-04-07 17:11:41 +0300

    init

---
+++ env.config.js
@@ -0,0 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  const result = require('dotenv').config();
+
+  if (result.error) {
+    throw result.error;
+  }
+
+  const { parsed: env } = result;
+  module.exports = env;
+} else {
+  module.exports = process.env;
+}
