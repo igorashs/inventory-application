1a5ac95584666a9e257ce5f0b7bc300cfde974e7
igorashs <igorashsirbu@gmail.com>
2020-04-10 13:27:39 +0300

    refactoring

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,44 +1,44 @@
 // home page (shows total data) on GET
-exports.index = (req, res) => {
+exports.getIndex = (req, res) => {
   res.send('/ not Implemented');
 };
 
 // list all categories on GET
-exports.categoryList = (req, res) => {
+exports.getCategoryList = (req, res) => {
   res.send('/categories not Implemented');
 };
 
 // display all items for a specific category GET
-exports.categoryItems = (req, res) => {
+exports.getCategoryItems = (req, res) => {
   res.send('/category/:id not Implemented');
 };
 
 // display category create form on GET
-exports.categoryCreateGet = (req, res) => {
+exports.getCategoryCreate = (req, res) => {
   res.send('/category/create GET not Implemented');
 };
 
 // handle category create on POST
-exports.categoryCreatePost = (req, res) => {
+exports.postCategoryCreate = (req, res) => {
   res.send('/category/create POST not Implemented');
 };
 
 // display category update form on GET
-exports.categoryUpdateGet = (req, res) => {
+exports.getCategoryUpdate = (req, res) => {
   res.send('/category/:id/update GET not Implemented');
 };
 
 // handle category update on POST
-exports.categoryUpdatePost = (req, res) => {
+exports.postCategoryUpdate = (req, res) => {
   res.send('/category/:id/update POST not Implemented');
 };
 
 // display category delete form on GET
-exports.categoryDeleteGet = (req, res) => {
+exports.getCategoryDelete = (req, res) => {
   res.send('/category/:id/delete GET not Implemented');
 };
 
 // handle category delete on POST
-exports.categoryDeletePost = (req, res) => {
+exports.postCategoryDelete = (req, res) => {
   res.send('/category/:id/delete POST not Implemented');
 };
--- routes/catalog.js
+++ routes/catalog.js
@@ -2,30 +2,30 @@ const router = require('express').Router();
 const categoryController = require('../controllers/categoryController');
 
 // GET catalog home page
-router.get('/', categoryController.index);
+router.get('/', categoryController.getIndex);
 
 // GET req for creating a category
-router.get('/category/create', categoryController.categoryCreateGet);
+router.get('/category/create', categoryController.getCategoryCreate);
 
 // POST req for creating a category
-router.post('/category/create', categoryController.categoryCreatePost);
+router.post('/category/create', categoryController.postCategoryCreate);
 
 // GET req to delete a category
-router.get('/category/:id/delete', categoryController.categoryDeleteGet);
+router.get('/category/:id/delete', categoryController.getCategoryDelete);
 
 // POST req to delete a category
-router.post('/category/:id/delete', categoryController.categoryDeletePost);
+router.post('/category/:id/delete', categoryController.postCategoryDelete);
 
 // GET req to update a category
-router.get('/category/:id/update', categoryController.categoryUpdateGet);
+router.get('/category/:id/update', categoryController.getCategoryUpdate);
 
 // POST req to update a category
-router.post('/category/:id/update', categoryController.categoryUpdatePost);
+router.post('/category/:id/update', categoryController.postCategoryUpdate);
 
 // GET req for all items from a specific category
-router.get('category/:id', categoryController.categoryItems);
+router.get('/category/:id', categoryController.getCategoryItems);
 
 // GET req for all categories
-router.get('/categories', categoryController.categoryList);
+router.get('/categories', categoryController.getCategoryList);
 
 module.exports = router;
