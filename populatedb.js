ab34f7214cef02467f1adf930d84d37c3e3a35c3
igorashs <igorashsirbu@gmail.com>
2020-04-14 21:06:18 +0300

    add comment

--- populatedb.js
+++ populatedb.js
@@ -1,3 +1,5 @@
+// THE IMGS SHOULD HAVE THE NAME OF document's ID
+
 console.log(
   'This script populates some test categories and items to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
 );
