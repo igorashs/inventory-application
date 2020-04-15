ba64600fd94d0d9bd29bef30717b5e4822a7bf86
igorashs <igorashsirbu@gmail.com>
2020-04-15 17:45:20 +0300

    add urlencoded mw

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.set('view engine', 'pug');
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
 // add our routers
 app.use('/', indexRouter);
 app.use('/catalog', catalogRouter);
